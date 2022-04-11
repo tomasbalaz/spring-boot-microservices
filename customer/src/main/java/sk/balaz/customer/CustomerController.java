99f7e650f4068729afbce26db0eb99217654d2fb
Tomas Balaz <tomasba@amdocs.com>
2022-04-11 13:53:30 +0200

    Created docker-compose.yml file with images postgres, dpage/pgadmin4

--- customer/src/main/java/sk/balaz/customer/CustomerController.java
+++ customer/src/main/java/sk/balaz/customer/CustomerController.java
@@ -1,6 +1,5 @@
 package sk.balaz.customer;
 
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
