9daae39a8fe381b3757fcf8b39b0610c8a8c2ce4
Tomas Balaz <tomasba@amdocs.com>
2022-04-12 13:33:20 +0200

    Created FraudCheckController and expose REST endpoint /api/v1/fraud-check/{customerId}

---
+++ fraud/src/main/java/sk/balaz/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package sk.balaz.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
--- fraud/src/main/java/sk/balaz/fraud/FraudCheckService.java
+++ fraud/src/main/java/sk/balaz/fraud/FraudCheckService.java
@@ -1,19 +1,16 @@
 package sk.balaz.fraud;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
+@AllArgsConstructor
 public class FraudCheckService {
 
     private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
-
-    @Autowired
-    public FraudCheckService(FraudCheckHistoryRepository fraudCheckHistoryRepository) {
-        this.fraudCheckHistoryRepository = fraudCheckHistoryRepository;
-    }
 
     public boolean isFraudulentCustomer(Integer customerId) {
         fraudCheckHistoryRepository.save(
