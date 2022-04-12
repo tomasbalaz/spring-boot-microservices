e84a929a6f56b16165ad5aa45c304e40239bf597
Tomas Balaz <tomasba@amdocs.com>
2022-04-12 10:06:12 +0200

    Updated Customer.java with JPA annotations

--- customer/src/main/java/sk/balaz/customer/Customer.java
+++ customer/src/main/java/sk/balaz/customer/Customer.java
@@ -1,12 +1,29 @@
 package sk.balaz.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Customer {
 
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
 
     private String firstName;
