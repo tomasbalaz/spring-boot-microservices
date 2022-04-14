a0cbf1ffb06e93abd9f2addeb9f61d161824e9d1
Tomas Balaz <tomasba@amdocs.com>
2022-04-14 11:34:05 +0200

    Created module clients with interface FraudClient as @FeignClient

---
+++ clients/src/main/java/sk/balaz/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package sk.balaz.clients.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ clients/src/main/java/sk/balaz/clients/fraud/FraudClient.java
@@ -0,0 +1,21 @@
+package sk.balaz.clients.fraud;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//v1
+//@FeignClient(
+//        value = "fraud",
+//        path = "/api/v1/fraud-check"
+//)
+
+//v2
+@FeignClient("fraud")
+public interface FraudClient {
+
+    @GetMapping(path = "/api/v1/fraud-check/{customerId}")
+    FraudCheckResponse isFraudster(
+            @PathVariable("customerId") Integer customerId);
+
+}
