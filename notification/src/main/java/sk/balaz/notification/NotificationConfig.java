7bb29ad01efe66d012fd051367b041c1f1f0852e
Tomas Balaz <tomasba@amdocs.com>
2022-04-20 15:02:39 +0200

    Created NotificationConfig class and add proper values into application.yml

---
+++ notification/src/main/java/sk/balaz/notification/NotificationConfig.java
@@ -0,0 +1,29 @@
+package sk.balaz.notification;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificationConfig {
+
+    @Value("${rabbitmq.exchanges.internal}")
+    private String internalExchange;
+
+    @Value("${rabbitmq.queue.notification}")
+    private String notificationQueue;
+
+    @Value("${rabbitmq.routing-keys.internal-notification}")
+    private String internalNotificationQueue;
+
+    public String getInternalExchange() {
+        return internalExchange;
+    }
+
+    public String getNotificationQueue() {
+        return notificationQueue;
+    }
+
+    public String getInternalNotificationQueue() {
+        return internalNotificationQueue;
+    }
+}
