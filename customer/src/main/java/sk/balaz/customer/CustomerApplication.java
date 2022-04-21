88f8135598d9e7c83abd564b07555b687fffcfd9
Tomas Balaz <tomasba@amdocs.com>
2022-04-21 13:54:55 +0200

    Updated customer microservice send message to RabbitMq

--- customer/src/main/java/sk/balaz/customer/CustomerApplication.java
+++ customer/src/main/java/sk/balaz/customer/CustomerApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "sk.balaz.customer",
+                "sk.balaz.amqp"
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "sk.balaz.clients"
--- customer/src/main/java/sk/balaz/customer/CustomerService.java
+++ customer/src/main/java/sk/balaz/customer/CustomerService.java
@@ -2,9 +2,9 @@ package sk.balaz.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import sk.balaz.amqp.RabbitMQMessageProducer;
 import sk.balaz.clients.fraud.FraudCheckResponse;
 import sk.balaz.clients.fraud.FraudClient;
-import sk.balaz.clients.notification.NotificationClient;
 import sk.balaz.clients.notification.NotificationRequest;
 
 @Service
@@ -15,7 +15,7 @@ public class CustomerService {
 
     private final FraudClient fraudClient;
 
-    private final NotificationClient notificationClient;
+    private final RabbitMQMessageProducer producer;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -37,15 +37,16 @@ public class CustomerService {
 
 
         // TODO: make it async. i.e add to queue
-        notificationClient.sendNotification(
-                new NotificationRequest(
-                        customer.getId(),
-                        customer.getEmail(),
-                        String.format("Hi %s, welcome in my microservices world...",
-                                customer.getFirstName())
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome in my microservices world...",
+                        customer.getFirstName())
 
-                )
         );
+        producer.publish(notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key");
 
     }
 }
