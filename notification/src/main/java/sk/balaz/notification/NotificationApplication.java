c8dd9088fc35f904e5091dd373de6ff45a5b9d01
Tomas Balaz <tomasba@amdocs.com>
2022-05-05 09:32:59 +0200

    Refactoring microservices customer, fraud, notification for getting new feign client properties

--- notification/src/main/java/sk/balaz/notification/NotificationApplication.java
+++ notification/src/main/java/sk/balaz/notification/NotificationApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import sk.balaz.amqp.RabbitMQMessageProducer;
 
 @SpringBootApplication(
@@ -14,6 +16,9 @@ import sk.balaz.amqp.RabbitMQMessageProducer;
         }
 )
 @EnableEurekaClient
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
 
     public static void main(String[] args) {
